a65db3f598b5c3ba2a6dce0d035e989e80187cb2
Shuban <67974101+shuban-789@users.noreply.github.com>
2023-10-09 09:45:06 -0700

    Create substitution.py

---
+++ substitution.py
@@ -0,0 +1,34 @@
+class substitution:
+  def __init__(self, key, text):
+    self.key = key
+    self.text = text
+  def encrypt(enc):
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ct=""
+    for i in range(0,len(enc.text)):
+      if enc.text[i] in alphabet:
+        for j in range(0,len(alphabet)):
+          if enc.text[i] == alphabet[j]:
+            index = j
+        newchar = enc.key[index]
+        ct += newchar
+      else:
+        ct += enc.text[i]
+    return ct
+
+  def decrypt(dec):
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    pt=""
+    for i in range(0,len(dec.text)):
+      if dec.text[i] in alphabet:
+        for j in range(0,len(dec.text)):
+          if dec.text[i] == dec.key[j]:
+            index = j
+        newchar = alphabet[index]
+        pt += newchar
+      else:
+        pt += dec.text[i]
+    return pt
+
+mycipher = substitution("QWERTYUIOPASDFGHJKLZXCVBNM","HELLO WORLD")
+print(mycipher.encrypt())
